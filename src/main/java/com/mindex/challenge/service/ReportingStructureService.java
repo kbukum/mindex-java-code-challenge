43794cf6ba0101d6c94d43c0a14359eed8a40510
kbukum <kamilbukum@gmail.com>
2021-03-19 13:19:07 -0400

    * removed unnecessary files.

--- src/main/java/com/mindex/challenge/service/ReportingStructureService.java
+++
@@ -1,7 +0,0 @@
-package com.mindex.challenge.service;
-
-import com.mindex.challenge.data.ReportingStructure;
-
-public interface ReportingStructureService {
-    ReportingStructure readByEmployeeId(String employeeId);
-}
